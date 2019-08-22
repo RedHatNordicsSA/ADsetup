066ae754e50a06cb29f9ff807eecac01d9aab1c5
Håkan Hagenrud <hakan@redhat.com>
2019-08-22 11:25:53 +0200

    now adds the users to active directory

--- setupad.ps1
+++ setupad.ps1
@@ -5,29 +5,21 @@ $myDomain = "hger.org"
 $myNetbios = "HGER"
 $myScript = "c:\usersgrops.ps1"
 
-Set-Runonce -command '%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass -file $myScript' 
-
+New-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce' -Name "Run" -Value '%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass -file $myScript' -PropertyType ExpandString
 $daScript = @'
-$Secure_String_Pwd = ConvertTo-SecureString $myPassword -AsPlainText -Force
-New-ADUser -Name "Hakan Hagenrud" -SamAccountName "hger" -Company "Purple Hat" -AccountPassword $Secure_String_Pwd
-New-ADUser -Name "Daniel Svensson" -SamAccountName "vsda" -Company "Furniture Heaven" -AccountPassword $Secure_String_Pwd
+$Secure_String_Pwd = ConvertTo-SecureString "Password1!" -AsPlainText -Force
+New-ADUser -Name "Hakan Hagenrud" -SamAccountName "hger" -Company "Purple Hat" -AccountPassword $Secure_String_Pwd -Enabled $true -ChangePasswordAtLogon $false
+New-ADUser -Name "Daniel Svensson" -SamAccountName "vsda" -Company "Furniture Heaven" -AccountPassword $Secure_String_Pwd -Enabled $true -ChangePasswordAtLogon $false
 '@
 
 echo $daScript > $myScript
 
 
-install-windowsfeature AD-Domain-Services
+Install-Windowsfeature AD-Domain-Services
+Install-WindowsFeature RSAT-ADDS
 Import-Module ADDSDeployment
 
-Install-ADDSForest -CreateDnsDelegation:$false ` 
-                   -DatabasePath "C:\Windows\NTDS" `
-                   -DomainMode "Win2012R2" `
-                   -DomainName $myDomain `
-                   -DomainNetbiosName $myNetbios `
-                   -ForestMode "Win2012R2" `
-                   -InstallDns:$true `
-                   -LogPath "C:\Windows\NTDS" `
-                   -NoRebootOnCompletion:$false `
-                   -SysvolPath "C:\Windows\SYSVOL" `
-                   -Force:$true `
-                   -SafeModeAdministratorPassword:$Secure_String_Pwd
+
+Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "Win2012R2" -DomainName $myDomain -DomainNetbiosName $myNetbios -ForestMode "Win2012R2" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\Windows\SYSVOL" -Force:$true -SafeModeAdministratorPassword:$Secure_String_Pwd ` 
+
+
